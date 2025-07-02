f3bf2d33b807aaba6946fa4262785c37762b861e
Rohith <717822e238@kce.ac.in>
2025-07-02 16:04:27 +0530

    Update and rename 2.java to AppTest2.java

--- AppTest2.java
+++ AppTest2.java
@@ -16,7 +16,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-public class AppTest {
+public class AppTest2 {
     WebDriver driver;
     WebDriverWait wait;
     Actions actions;
